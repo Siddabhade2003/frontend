8dc78ae733aa7c85174a825e41a6fe2bd7b77323
Siddabhade2003 <144590951+Siddabhade2003@users.noreply.github.com>
2024-08-25 01:00:25 +0530

    Update CourseInstanceDetails.js

--- src/components/CourseInstanceDetails.js
+++ src/components/CourseInstanceDetails.js
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams, Link } from 'react-router-dom';
-import './CourseInstanceDetails.css'; // Add your custom styles here
+import './CourseInstanceDetails.css'; 
 
 const InstanceDetails = () => {
     const { year, semester, id } = useParams();
