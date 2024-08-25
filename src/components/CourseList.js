19648739c6d552293dba3cbae9d70b88f9077f51
Siddabhade2003 <144590951+Siddabhade2003@users.noreply.github.com>
2024-08-25 11:11:24 +0530

    Update CourseList.js

--- src/components/CourseList.js
+++ src/components/CourseList.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css'; // Import the toast styles
+import 'react-toastify/dist/ReactToastify.css'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const CourseList = () => {
